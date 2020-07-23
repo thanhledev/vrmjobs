a5109d387d10474b8259f6bd871c4fa48f567bd5
thanhle <thanhledev@gmail.com>
2020-07-24 00:01:47 +0200

    Add setup.py && fix probe jobs

--- vrmjobs/probe_ack.py
+++ vrmjobs/probe_ack.py
@@ -1,4 +1,5 @@
 from .worker_info import WorkerInfo
+from .vrm_type import VrmType
 
 class ProbeAck(object):
     """
@@ -6,12 +7,13 @@ class ProbeAck(object):
     and reply back to collector host using information provided
     in probe_init packet
     """
-    def __init__(self, packet_id: str, info: 'WorkerInfo'):
+    def __init__(self, packet_id: str, info: 'WorkerInfo', packet_type: 'VrmType'):
         self.id = packet_id
         self.info = info
+        self.type = packet_type
 
     def __str__(self):
-        return "{} - {}".format(id, str(self.info))
+        return "[{}] {} - {}".format(str(self.type), id, str(self.info))
 
     def __repr__(self):
         return "{}({} - {})".format(self.__class__.__name__,
--- vrmjobs/probe_init.py
+++ vrmjobs/probe_init.py
@@ -6,13 +6,13 @@ class ProbeInit(object):
     System packet that will be encapsulated inside an UDP packet
     and broadcast to all worker hosts inside a single network segment
     """
-    def __init__(self, packet_id: str, info: 'CollectorInfo', type: 'VrmType'):
+    def __init__(self, packet_id: str, info: 'CollectorInfo', packet_type: 'VrmType'):
         self.id = packet_id
         self.info = info
-        self.type = type
+        self.type = packet_type
 
     def __str__(self):
-        return "{} - {} - {}".format(id, str(type), str(self.info))
+        return "[{}] {} - {}".format(str(self.type), id, str(self.info))
 
     def __repr__(self):
         return "{}({} - {})".format(self.__class__.__name__,
