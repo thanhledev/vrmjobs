76b23a4635aaa4cef5035eea4dba16ccf3a08a38
thanhle <thanhledev@gmail.com>
2020-07-24 11:01:26 +0200

    Update worker_info.py (add hostname to class' properties)

--- vrmjobs/worker_info.py
+++ vrmjobs/worker_info.py
@@ -3,18 +3,21 @@ class WorkerInfo(object):
     Define structure of the information of a worker host that
     will be exchanged via probe_ack packet
     """
-    def __init__(self, ip_addr:str, port: int, exporter_info: {}):
+    def __init__(self, ip_addr:str, port: int, hostname: str, exporter_info: {}):
         self.ip_addr = ip_addr
         self.port = port
+        self.hostname = hostname
         self.exporter_info = exporter_info
 
     def __str__(self):
-        return "{}:{} - exporters: {}".format(self.ip_addr,
-                                              self.port,
-                                              self.exporter_info)
-
-    def __repr__(self):
-        return "{}({}:{}) - exporters: {}".format(self.__class__.__name__,
+        return "{}[{}:{}] - exporters: {}".format(self.hostname,
                                                   self.ip_addr,
                                                   self.port,
                                                   self.exporter_info)
+
+    def __repr__(self):
+        return "{}({}[{}:{}] - exporters: {})".format(self.__class__.__name__,
+                                                      self.hostname,
+                                                      self.ip_addr,
+                                                      self.port,
+                                                      self.exporter_info)
