d7f65a63ed37021269eb2631739c36e118ff9856
thanhle <thanhledev@gmail.com>
2020-07-25 22:34:14 +0200

    Fix missing imports

--- vrmjobs/__init__.py
+++ vrmjobs/__init__.py
@@ -2,5 +2,7 @@ from .vrm_type import VrmType
 from .port_info import PortInfo
 from .worker_info import WorkerInfo
 from .collector_info import CollectorInfo
+from .metric_info import MetricInfo
 from .probe_ack import ProbeAck
 from .probe_init import ProbeInit
+from .metric_init import MetricInit
