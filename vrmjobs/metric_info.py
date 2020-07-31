c6ec05737f835a551221faaafe2569d2649af58a
thanhle <thanhledev@gmail.com>
2020-08-01 01:30:23 +0200

    Edit metric_info class: add new field named 'hostname'

--- vrmjobs/metric_info.py
+++ vrmjobs/metric_info.py
@@ -6,23 +6,27 @@ class MetricInfo(object):
     Define structure of the information of the metric exporter
     running in a worker host
     """
-    def __init__(self, inet_addr: str, ports: List[PortInfo]):
+    def __init__(self, hostname: str, inet_addr: str, ports: List[PortInfo]):
+        self.hostname = hostname
         self.inet_addr = inet_addr
         self.ports = ports
 
     def __str__(self):
-        return "{} - ports: {}".format(self.inet_addr,
-                                       str(self.ports))
-
-    def __repr__(self):
-        return "{}({} - ports: {})".format(self.__class__.__name__,
+        return "{}[{}] - ports: {}".format(self.hostname,
                                            self.inet_addr,
                                            str(self.ports))
+
+    def __repr__(self):
+        return "{}({}[{}] - ports: {})".format(self.__class__.__name__,
+                                               self.hostname,
+                                               self.inet_addr,
+                                               str(self.ports))
 
     def metric_fqinfo(self) -> List[Dict]:
         metrics = []
         for info in self.ports:
             metrics.append({'job': info.daemon,
-                            'target': "{}:{}".format(self.inet_addr, info.port)})
+                            'hostname': self.hostname,
+                            'endpoint': "{}:{}".format(self.inet_addr, info.port)})
 
         return metrics
